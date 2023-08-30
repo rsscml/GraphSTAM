992d292cd9660bbff9a8c22a9a4c7a8125eacf4c
rsscml <rahul.sinhaocp@gmail.com>
2023-08-30 20:26:23 +0530

    added baseline forecast support

--- SpatialTemporalGraph/GraphModelLib_v5_SpatialTemporal.py
+++ SpatialTemporalGraph/GraphModelLib_v5_SpatialTemporal.py
@@ -2236,7 +2236,9 @@ class graphmodel():
             return total_loss / total_examples
         
         def test_fn():
-            self.model.train(False)
+            #self.model.train(False) # original line
+            self.model.eval()
+            self.model.lstm_stack.train()
             total_examples = 0 
             total_loss = 0
             with torch.no_grad(): 
@@ -2377,7 +2379,8 @@ class graphmodel():
         def infer_fn(model, model_path, infer_dataset):
             model.load_state_dict(torch.load(model_path))
             model.eval()
-            model.train(False)
+            model.lstm_stack.train()  # -- added as a workaround for captum --30/08/23 -- remove after testing
+            #model.train(False)
             output = []
             with torch.no_grad(): 
                 for i, batch in enumerate(infer_dataset):
