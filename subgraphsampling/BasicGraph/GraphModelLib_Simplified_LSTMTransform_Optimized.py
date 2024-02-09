3aa29d68e8dddc6528a6e56c4cc4dcd0de8b9bb3
rsscml <rahul.sinhaocp@gmail.com>
2024-02-09 16:09:14 +0530

    probabilistic forecasts

--- subgraphsampling/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
+++ subgraphsampling/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
@@ -1429,8 +1429,10 @@ class graphmodel():
             snap_periods_list = sorted(df[self.time_index_col].unique(), reverse=False)
 
             # restrict samples for very large datasets based on interleaving
+            if df_type == 'train':
+                snap_periods_list = snap_periods_list[int(self.max_lags - 1):]
+
             if (self.interleave > 1) and (df_type == 'train'):
-                snap_periods_list = snap_periods_list[int(self.max_lags-1):]
                 snap_periods_list = snap_periods_list[0::self.interleave] + [snap_periods_list[-1]]
             
             print("picking {} samples for {}".format(len(snap_periods_list), df_type))
