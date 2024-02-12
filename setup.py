cbebd409affe3ca1c64baa09392d98ae9d223667
rsscml <rahul.sinhaocp@gmail.com>
2024-02-12 16:11:02 +0530

    probabilistic forecasts

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="GraphSTAM",
-    version="1.2.5",
+    version="1.2.6",
     description="Graph Based Spatio-Temporal Attention Models For Demand Forecasting",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,7 +17,8 @@ setup(
     author_email="rahul.sinha@unilever.com",
     packages=["graphstam", "BasicGraph", "SpatialTemporalGraph", "TemporalSpatialGraph", "optimized.graphstam",
               "optimized.BasicGraph", "optimized.SpatialTemporalGraph", "optimized.TemporalSpatialGraph",
-              "probabilistic.graphstam", "probabilistic.BasicGraph", "subgraphsampling.graphstam", "subgraphsampling.BasicGraph"],
+              "probabilistic.graphstam", "probabilistic.BasicGraph", "subgraphsampling.graphstam", "subgraphsampling.BasicGraph",
+              "smallgraph.graphstam", "smallgraph.BasicGraph"],
     include_package_data=True,
     install_requires=[]
 )
