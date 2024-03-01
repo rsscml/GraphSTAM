37f8cd0200c90f002ca9833682ba2b2dfd8a6e40
rsscml <rahul.sinhaocp@gmail.com>
2024-03-01 20:53:28 +0530

    keybom

--- hierarchical/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
+++ hierarchical/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
@@ -832,7 +832,7 @@ class STGNN(torch.nn.Module):
             print("out, before aggregation: ")
             print(out[:5])
             for i in range(out.shape[0]):
-                out[i] = torch.index_select(out, 0, torch.Variable(keybom[i][keybom[i] != -1])).sum(dim=0)
+                out[i] = torch.index_select(out, 0, torch.autograd.Variable(keybom[i][keybom[i] != -1])).sum(dim=0)
             out.retain_grad()
             print("out, after aggregation: ")
             print(out[:5])
