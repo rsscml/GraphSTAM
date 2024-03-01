e21671b37702d686907ae2ba6191579743e24a51
rsscml <rahul.sinhaocp@gmail.com>
2024-03-02 02:38:41 +0530

    keybom

--- hierarchical/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
+++ hierarchical/BasicGraph/GraphModelLib_Simplified_LSTMTransform_Optimized.py
@@ -1368,7 +1368,7 @@ class graphmodel():
         # convert 'key_list' to key indices
         #print([ [col_map_dict[self.id_col]['index'][k] for k in row if col_map_dict[self.id_col]['index'].get(k)] for row in df_snap['key_list']])
         print([literal_eval(row) for row in df_snap.key_list])
-        df_snap = df_snap.assign(mapped_key_list=[[col_map_dict[self.id_col]['index'][k] for k in row if col_map_dict[self.id_col]['index'].get(k)] for literal_eval(row) in df_snap['key_list']])
+        df_snap = df_snap.assign(mapped_key_list=[[col_map_dict[self.id_col]['index'][k] for k in literal_eval(row) if col_map_dict[self.id_col]['index'].get(k)] for row in df_snap['key_list']])
         print(df_snap[[self.id_col, 'key_list', 'mapped_key_list']].head())
         df_snap['mapped_key_list_arr'] = df_snap['mapped_key_list'].apply(lambda x: np.array(x))
         keybom_nested = torch.nested.nested_tensor(list(df_snap['mapped_key_list_arr'].values), dtype=torch.int64)
