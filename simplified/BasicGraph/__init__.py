98b398239076102fa8276ff803f79ba78aa6812d
rsscml <rahul.sinhaocp@gmail.com>
2024-05-09 10:15:13 +0530

    mem opt

--- simplified/BasicGraph/__init__.py
+++ simplified/BasicGraph/__init__.py
@@ -1,4 +1,4 @@
-#from simplified.BasicGraph.GraphModelLib_Simplified import graphmodel
-from simplified.BasicGraph.GraphModelLib_Simplified_mixedvalidation import graphmodel
+from simplified.BasicGraph.GraphModelLib_Simplified import graphmodel
+#from simplified.BasicGraph.GraphModelLib_Simplified_mixedvalidation import graphmodel
 
 
