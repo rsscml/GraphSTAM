bba0fb8a1727857b487fb1964319f3d74467f6ba
rsscml <rahul.sinhaocp@gmail.com>
2024-05-17 19:27:15 +0530

    mem opt

--- simplified/HierarchicalGraph/__init__.py
+++ simplified/HierarchicalGraph/__init__.py
@@ -1,2 +1,3 @@
-from simplified.HierarchicalGraph.GraphModelLib_Simplified_Hierarchical import graphmodel
+#from simplified.HierarchicalGraph.GraphModelLib_Simplified_Hierarchical import graphmodel
+from simplified.HierarchicalGraph.GraphModelLib_Simplified_Hierarchical_MixedValidation import graphmodel
 
