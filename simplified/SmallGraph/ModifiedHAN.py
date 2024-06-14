d4d0ca2e7b619228e59ad072241f2730cd97b0dd
rsscml <rahul.sinhaocp@gmail.com>
2024-06-14 19:39:29 +0530

    mem opt

--- simplified/SmallGraph/ModifiedHAN.py
+++ simplified/SmallGraph/ModifiedHAN.py
@@ -192,6 +192,7 @@ class ModHANConv(MessagePassing):
                     else:
                         # updated node embeddings
                         x_node_dict[node_type] = outs
+                        print("layer > 0, outs shape", node_type, len(outs), outs[0].shape)
 
                 # reset
                 for node_type, x in x_dict.items():
@@ -205,7 +206,7 @@ class ModHANConv(MessagePassing):
                         lin_dst = self.conv_layers[f"{i}_dst"][edge_type]
                         x_src = x_node_dict[src_type]
                         x_dst = x_node_dict[dst_type]
-                        print("layer >0, src & dst shapes: ", x_src.shape, x_dst.shape)
+                        print("layer >0, src & dst shapes: ", len(x_src), x_src[0].shape)
                         alpha_src = (x_src * lin_src).sum(dim=-1)
                         alpha_dst = (x_dst * lin_dst).sum(dim=-1)
                         # propagate_type: (x: PairTensor, alpha: PairTensor)
