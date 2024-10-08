a4fa1bc98e87715cb319893f94fba472cfa4dc48
rsscml <rahul.sinhaocp@gmail.com>
2024-10-08 17:37:41 +0530

    new multistep hierarchical model

--- simplified/MultistepHierarchicalGraph/GraphModelLib_Simplified_MultistepHierarchical_RecursivePredict.py
+++ simplified/MultistepHierarchicalGraph/GraphModelLib_Simplified_MultistepHierarchical_RecursivePredict.py
@@ -2606,6 +2606,7 @@ class graphmodel:
                 # key weight
                 if sample_weights:
                     wt = torch.unsqueeze(batch[self.target_col].y_weight, dim=2)
+                    print("wt shape: ", wt.shape)
                 else:
                     wt = 1
 
@@ -2615,8 +2616,13 @@ class graphmodel:
                 # recency wt
                 if self.recency_weights:
                     recency_wt = torch.unsqueeze(batch[self.target_col].recency_weight, dim=2)
+                    print("recency shape: ", recency_wt.shape)
                 else:
                     recency_wt = 1
+
+                print("loss shape: ", loss.shape)
+                print("mask shape: ", mask.shape)
+                print("key_level_wt shape: ", key_level_wt.shape)
 
                 weighted_loss = torch.mean(loss * mask * wt * key_level_wt * recency_wt)
 
